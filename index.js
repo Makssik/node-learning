42d2a19f2c84e36df0f9cce7c09f95741b494f1d
Makssik <maksik07052004@gmail.com>
2022-12-04 13:24:33 +0200

    Небольшие изменения, добавил корс

--- index.js
+++ index.js
@@ -9,28 +9,32 @@ import express from 'express';
 import morgan from 'morgan';
 import { got } from 'got';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 app.use(morgan('tiny'));
+app.use(cors())
 
 const PORT = process.env.PORT;
 const BASE_URL = 'https://api.weatherbit.io/v2.0/current';
 
-app.get('/api/weather/:city', async (req, res) => {
+app.get('/api/weather', async (req, res) => {
   try {
+    const { city_to_search } = req.query;
+
     const options = {
       searchParams: {
         key: process.env.WEATHER_API_KEY,
-        city: req.params.city,
+        city: city_to_search,
         lang: 'uk',
       },
     };
     const response = await got(BASE_URL, options).json();
 
-    const { temp, city_name, weather } = response.data[0];
+    const { temp, city_name, weather: {description} } = response.data[0];
     res.end(
-      `В городе ${city_name} погода ${weather.description} и температура ${temp} градусов`,
+      `В городе ${city_name} погода ${description} и температура ${temp} градусов`,
     );
   } catch (err) {
     res.json({ err: err.message });
